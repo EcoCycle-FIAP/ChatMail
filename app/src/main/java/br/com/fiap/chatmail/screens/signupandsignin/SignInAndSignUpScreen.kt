675a921660aa037193d11978746085a43e9d7aab
adrian-siv <adrian.ssiv@gmail.com>
2024-06-16 13:32:11 -0300

    fix: implement EmailCard on MailBoxScreen

--- app/src/main/java/br/com/fiap/chatmail/screens/signupandsignin/SignInAndSignUpScreen.kt
+++ app/src/main/java/br/com/fiap/chatmail/screens/signupandsignin/SignInAndSignUpScreen.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import br.com.fiap.chatmail.screens.mailbox.MailBoxScreen
+import br.com.fiap.chatmail.components.TabBar
 import br.com.fiap.chatmail.screens.signupandsignin.components.SignInScreen
 import br.com.fiap.chatmail.screens.signupandsignin.components.SignUpScreen
 
@@ -26,7 +26,7 @@ fun SignInAndSignUpScreen() {
             )
         }
         composable(route = "mailbox"){
-            MailBoxScreen()
+            TabBar()
         }
     }
 }
