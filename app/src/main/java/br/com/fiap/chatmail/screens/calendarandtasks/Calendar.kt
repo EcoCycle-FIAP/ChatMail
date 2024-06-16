a08bdae286c5f71dde9cda5f3184437f2ce5adaf
Guilherme Indiciate <Guilherme.Indiciate@ibm.com>
2024-06-16 12:06:52 -0300

    adds

--- app/src/main/java/br/com/fiap/chatmail/screens/calendarandtasks/Calendar.kt
+++ app/src/main/java/br/com/fiap/chatmail/screens/calendarandtasks/Calendar.kt
@@ -1,20 +1,16 @@
 package br.com.fiap.chatmail.screens.calendarandtasks
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
--- app/src/main/java/br/com/fiap/chatmail/screens/chatscreenmail/ChatScreenSending.kt
+++ app/src/main/java/br/com/fiap/chatmail/screens/chatscreenmail/ChatScreenSending.kt
@@ -25,10 +25,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
