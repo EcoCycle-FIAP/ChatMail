e0a9834f163b06b8c3bccfd17002b8033e8149cb
adrian-siv <adrian.ssiv@gmail.com>
2024-06-17 21:29:57 -0300

    feat: implement FavoritesScreen

--- app/src/main/java/br/com/fiap/chatmail/screens/favorites/FavoritesScreen.kt
+++ app/src/main/java/br/com/fiap/chatmail/screens/favorites/FavoritesScreen.kt
@@ -1,30 +1,45 @@
 package br.com.fiap.chatmail.screens.favorites
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import br.com.fiap.chatmail.R
 import br.com.fiap.chatmail.components.TabBar
+import br.com.fiap.chatmail.screens.favorites.components.EmailCard
+import br.com.fiap.chatmail.screens.favorites.components.NewEmailButton
 
 @Composable
 fun FavoritesScreen(navController: NavController) {
-    Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(bottom = 20.dp)
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.SpaceBetween
-    ){
+    Column(
+    ) {
         TabBar(navController = navController)
-        Text(text = "Tela de Favoritos")
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(colorResource(id = R.color.chatmail_lightgray_color))
+        ) {
+            LazyColumn(
+                modifier = Modifier.background(color = colorResource(id = R.color.chatmail_lightgray_color))
+            ) {
+                items(4) {
+                    EmailCard(iteration = it, navController = navController)
+                }
+            }
+            NewEmailButton(
+                navController = navController,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 25.dp)
+            )
+        }
     }
 }
---
+++ app/src/main/java/br/com/fiap/chatmail/screens/favorites/components/EmailCard.kt
@@ -0,0 +1,103 @@
+package br.com.fiap.chatmail.screens.favorites.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import br.com.fiap.chatmail.R
+import br.com.fiap.chatmail.ui.theme.Jaldi
+
+@Composable
+fun EmailCard(iteration: Int, navController: NavController) {
+
+    //Lógica para simular diferentes cores de avatares
+    var avatarColor = R.color.primary_color
+
+    when (iteration) {
+        0, 2 -> {
+            avatarColor = R.color.chatmail_red_color
+        }
+        1, 3 -> {
+            avatarColor = R.color.chatmail_green_color
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(vertical = 10.dp, horizontal = 25.dp)
+            .fillMaxWidth()
+            .height(180.dp)
+            .clickable { navController.navigate("expandedemail") },
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()
+        ) {
+            Box(Modifier.weight(0.2f), contentAlignment = Alignment.CenterStart) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_account_circle_24),
+                    contentDescription = "Avatar do usuário",
+                    modifier = Modifier.size(55.dp),
+                    tint = colorResource(id = avatarColor)
+                )
+            }
+            Column(Modifier.weight(0.55f)) {
+                Text(
+                    text = "Nome do Usuário",
+                    fontSize = 18.sp,
+                    fontFamily = Jaldi,
+                    lineHeight = 10.sp
+                )
+                Text(
+                    text = "Assunto do Email",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = Jaldi
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.weight(0.25f)
+            ) {
+                Text(
+                    text = "XXt atrás",
+                    fontSize = 16.sp,
+                    fontFamily = Jaldi,
+                    color = colorResource(id = R.color.chatmail_gray_color),
+                    textAlign = TextAlign.End
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_favorite_24),
+                    contentDescription = "Ícone de coração para favoritar o email.",
+                    tint = colorResource(id = R.color.chatmail_yellow_color)
+                )
+            }
+        }
+        Text(
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis semper nibh, eget tincidunt nunc convallis vel. Cras feugiat erat vitae efficitur hendrerit. Aenean tristique ex posuere ligula accumsan ultrices. Ut suscipit justo orci, sit amet pulvinar ipsum pharetra quis.",
+            fontFamily = Jaldi,
+            color = colorResource(
+                id = R.color.chatmail_gray_color
+            )
+        )
+    }
+    HorizontalDivider(color = colorResource(id = R.color.chatmail_gray_color), modifier = Modifier.padding(horizontal = 25.dp))
+}
