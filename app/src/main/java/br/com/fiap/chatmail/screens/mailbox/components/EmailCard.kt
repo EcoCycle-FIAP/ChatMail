dad7c7f621611e8fd8882c329570cbba8cf4804b
adrian-siv <adrian.ssiv@gmail.com>
2024-09-18 23:51:12 -0300

    feat: add delete button

--- app/src/main/java/br/com/fiap/chatmail/screens/mailbox/components/EmailCard.kt
+++ app/src/main/java/br/com/fiap/chatmail/screens/mailbox/components/EmailCard.kt
@@ -1,5 +1,6 @@
 package br.com.fiap.chatmail.screens.mailbox.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,12 +10,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -109,6 +115,37 @@ fun EmailCard(iteration: Int, navController: NavController, email: Email) {
                 id = R.color.chatmail_gray_color
             )
         )
+        DeleteButton(onClick = { /*TODO*/ })
     }
     HorizontalDivider(color = colorResource(id = R.color.chatmail_gray_color), modifier = Modifier.padding(horizontal = 25.dp))
+}
+
+@Composable
+fun DeleteButton(
+    onClick: () -> Unit,
+    iconSize: Int = 24,
+    width: Int? = 40,
+    height: Int? = 40,
+    borderColor: Int = R.color.chatmail_red_color, // Usar a cor para a borda
+    iconColor: Int = R.color.chatmail_red_color // Cor do ícone
+) {
+    OutlinedButton(
+        onClick = onClick,
+        modifier = Modifier
+            .width(width?.dp ?: 40.dp)
+            .height(height?.dp ?: 40.dp),
+        shape = RoundedCornerShape(12.dp),
+        colors = ButtonDefaults.outlinedButtonColors(
+            containerColor = Color.Transparent, // Fundo transparente
+            contentColor = colorResource(id = iconColor) // Cor do ícone
+        ),
+        border = BorderStroke(1.dp, colorResource(id = borderColor)) // Define a cor da borda
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.trash_24),
+            contentDescription = "Deletar",
+            modifier = Modifier.size(iconSize.dp),
+            tint = colorResource(id = iconColor) // Define a cor do ícone
+        )
+    }
 }
--- app/src/main/java/br/com/fiap/chatmail/screens/newemail/NewEmailScreen.kt
+++ app/src/main/java/br/com/fiap/chatmail/screens/newemail/NewEmailScreen.kt
@@ -19,14 +19,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.testing.TestNavHostController
 import br.com.fiap.chatmail.R
 import br.com.fiap.chatmail.backend.database.RetrofitClient
 import br.com.fiap.chatmail.backend.model.Email
@@ -130,16 +126,5 @@ fun sendEmail(email: Email) {
             println("Erro inesperado: ${e.message}")
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewMyComposable() {
-
-    val navController = TestNavHostController(LocalContext.current)
-
-    val fakeViewModel = NewEmailScreenViewModel()
-
-    NewEmailScreen(navController = navController, newEmailScreenViewModel = fakeViewModel)
 }
 
