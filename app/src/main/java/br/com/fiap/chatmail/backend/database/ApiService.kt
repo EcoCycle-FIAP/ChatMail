5bcdf362005a2d0e4449a908053bac93f7f9fe6c
adrian-siv <adrian.ssiv@gmail.com>
2024-09-18 22:00:34 -0300

    feat: add deleteEmail endpoint

--- app/src/main/java/br/com/fiap/chatmail/backend/database/ApiService.kt
+++ app/src/main/java/br/com/fiap/chatmail/backend/database/ApiService.kt
@@ -3,8 +3,10 @@ package br.com.fiap.chatmail.backend.database
 import br.com.fiap.chatmail.backend.model.Email
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("emails")
@@ -12,4 +14,7 @@ interface ApiService {
 
     @POST("emails")
     suspend fun sendEmail(@Body email: Email): Response<Unit>
+
+    @DELETE("emails/{id}")
+    suspend fun deleteEmail(@Path("id") emailId: Int): Response<Unit>
 }
